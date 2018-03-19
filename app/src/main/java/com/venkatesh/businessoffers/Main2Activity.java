0a46f3bea10cf388a1d88c12ebd46332421b18d5
venkatesh-u <venkatesh.uppalapu@gmail.com>
2018-03-19 19:31:42 +0530

    iniate add offer

--- app/src/main/java/com/venkatesh/businessoffers/Main2Activity.java
+++ app/src/main/java/com/venkatesh/businessoffers/Main2Activity.java
@@ -81,8 +81,11 @@ public class Main2Activity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
+        if (id == R.id.nav_offers) {
+
+
+
+
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
